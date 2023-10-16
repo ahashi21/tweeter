dcbc66550aaee5830ae4290bd66d44d40cc46c7e
ahashi21 <ahmedxusseinx@gmail.com>
2023-10-15 22:47:36 -0400

    fetch with ajax added

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,35 +7,35 @@
 $(document).ready(function() {
 
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
+  // const data = [
+  //   {
+  //     "user": {
+  //       "name": "Newton",
+  //       "avatars": "https://i.imgur.com/73hZDYK.png"
+  //       ,
+  //       "handle": "@SirIsaac"
+  //     },
+  //     "content": {
+  //       "text": "If I have seen further it is by standing on the shoulders of giants"
+  //     },
+  //     "created_at": 1461116232227
+  //   },
+  //   {
+  //     "user": {
+  //       "name": "Descartes",
+  //       "avatars": "https://i.imgur.com/nlhLi3I.png",
+  //       "handle": "@rd" },
+  //     "content": {
+  //       "text": "Je pense , donc je suis"
+  //     },
+  //     "created_at": 1461113959088
+  //   }
+  // ];
 
   $("#new-tweet-form").submit(function(event) {
     event.preventDefault();
-    const tweet = $(this).serialize();
-    $.post("/tweets/", tweet);
+    const newTweet = $(this).serialize();
+    $.post("/tweets/", newTweet);
   });
 
   const renderTweets = function(tweets) {
@@ -73,5 +73,11 @@ $(document).ready(function() {
     return $tweet;
   };
 
-  renderTweets(data);
+  const loadTweets = function() {
+    $.get("/tweets/", function(newTweet) {
+      renderTweets(newTweet);
+    });
+  };
+
+  loadTweets();
 });
